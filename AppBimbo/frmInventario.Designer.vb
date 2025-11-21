43cc03ff8f9c5d4de510c85a1796ce9a50a5658f
pedromendo153-creator <pedromendo153@gmail.com>
2025-11-21 10:30:52 -0500

    Agregando formulario Inventario

---
+++ AppBimbo/frmInventario.Designer.vb
@@ -0,0 +1,269 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmInventario
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmInventario))
+        txtStockMinimo = New TextBox()
+        txtIdProducto = New TextBox()
+        txtIdAlmacen = New TextBox()
+        txtIdInventario = New TextBox()
+        txtCantidadStock = New TextBox()
+        txtStockMaximo = New TextBox()
+        dtpFechaActualizacion = New DateTimePicker()
+        btnNuevo = New Button()
+        btnGuardar = New Button()
+        btnEditar = New Button()
+        btnEliminar = New Button()
+        btnCerrar = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        dgvInventario = New DataGridView()
+        CType(dgvInventario, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' txtStockMinimo
+        ' 
+        txtStockMinimo.Location = New Point(140, 235)
+        txtStockMinimo.Name = "txtStockMinimo"
+        txtStockMinimo.Size = New Size(125, 27)
+        txtStockMinimo.TabIndex = 0
+        ' 
+        ' txtIdProducto
+        ' 
+        txtIdProducto.Location = New Point(140, 80)
+        txtIdProducto.Name = "txtIdProducto"
+        txtIdProducto.Size = New Size(125, 27)
+        txtIdProducto.TabIndex = 1
+        ' 
+        ' txtIdAlmacen
+        ' 
+        txtIdAlmacen.Location = New Point(140, 124)
+        txtIdAlmacen.Name = "txtIdAlmacen"
+        txtIdAlmacen.Size = New Size(125, 27)
+        txtIdAlmacen.TabIndex = 2
+        ' 
+        ' txtIdInventario
+        ' 
+        txtIdInventario.Location = New Point(140, 34)
+        txtIdInventario.Name = "txtIdInventario"
+        txtIdInventario.ReadOnly = True
+        txtIdInventario.Size = New Size(125, 27)
+        txtIdInventario.TabIndex = 3
+        ' 
+        ' txtCantidadStock
+        ' 
+        txtCantidadStock.Location = New Point(140, 172)
+        txtCantidadStock.Name = "txtCantidadStock"
+        txtCantidadStock.Size = New Size(125, 27)
+        txtCantidadStock.TabIndex = 4
+        ' 
+        ' txtStockMaximo
+        ' 
+        txtStockMaximo.Location = New Point(140, 282)
+        txtStockMaximo.Name = "txtStockMaximo"
+        txtStockMaximo.Size = New Size(125, 27)
+        txtStockMaximo.TabIndex = 5
+        ' 
+        ' dtpFechaActualizacion
+        ' 
+        dtpFechaActualizacion.Location = New Point(152, 336)
+        dtpFechaActualizacion.Name = "dtpFechaActualizacion"
+        dtpFechaActualizacion.Size = New Size(250, 27)
+        dtpFechaActualizacion.TabIndex = 6
+        ' 
+        ' btnNuevo
+        ' 
+        btnNuevo.Location = New Point(15, 390)
+        btnNuevo.Name = "btnNuevo"
+        btnNuevo.Size = New Size(94, 29)
+        btnNuevo.TabIndex = 7
+        btnNuevo.Text = "Nuevo"
+        btnNuevo.UseVisualStyleBackColor = True
+        ' 
+        ' btnGuardar
+        ' 
+        btnGuardar.Location = New Point(171, 390)
+        btnGuardar.Name = "btnGuardar"
+        btnGuardar.Size = New Size(94, 29)
+        btnGuardar.TabIndex = 8
+        btnGuardar.Text = "Guardar"
+        btnGuardar.UseVisualStyleBackColor = True
+        ' 
+        ' btnEditar
+        ' 
+        btnEditar.Location = New Point(308, 390)
+        btnEditar.Name = "btnEditar"
+        btnEditar.Size = New Size(94, 29)
+        btnEditar.TabIndex = 9
+        btnEditar.Text = "Editar"
+        btnEditar.UseVisualStyleBackColor = True
+        ' 
+        ' btnEliminar
+        ' 
+        btnEliminar.Location = New Point(560, 390)
+        btnEliminar.Name = "btnEliminar"
+        btnEliminar.Size = New Size(94, 29)
+        btnEliminar.TabIndex = 10
+        btnEliminar.Text = "Eliminar"
+        btnEliminar.UseVisualStyleBackColor = True
+        ' 
+        ' btnCerrar
+        ' 
+        btnCerrar.Location = New Point(441, 390)
+        btnCerrar.Name = "btnCerrar"
+        btnCerrar.Size = New Size(94, 29)
+        btnCerrar.TabIndex = 11
+        btnCerrar.Text = "Cerrar"
+        btnCerrar.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 34)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(94, 20)
+        Label1.TabIndex = 12
+        Label1.Text = "ID Inventario"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(12, 83)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(88, 20)
+        Label2.TabIndex = 13
+        Label2.Text = "ID Producto"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 124)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(86, 20)
+        Label3.TabIndex = 14
+        Label3.Text = "ID Almacén"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(-3, 179)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(109, 20)
+        Label4.TabIndex = 15
+        Label4.Text = "Cantidad Stock"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(6, 235)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(100, 20)
+        Label5.TabIndex = 16
+        Label5.Text = "Stock Mínimo"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(6, 285)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(103, 20)
+        Label6.TabIndex = 17
+        Label6.Text = "Stock Máximo"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(6, 340)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(140, 20)
+        Label7.TabIndex = 18
+        Label7.Text = "Fecha Actualización"
+        ' 
+        ' dgvInventario
+        ' 
+        dgvInventario.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvInventario.Location = New Point(330, 44)
+        dgvInventario.Name = "dgvInventario"
+        dgvInventario.RowHeadersWidth = 51
+        dgvInventario.Size = New Size(458, 261)
+        dgvInventario.TabIndex = 19
+        ' 
+        ' frmInventario
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(800, 450)
+        Controls.Add(dgvInventario)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(btnCerrar)
+        Controls.Add(btnEliminar)
+        Controls.Add(btnEditar)
+        Controls.Add(btnGuardar)
+        Controls.Add(btnNuevo)
+        Controls.Add(dtpFechaActualizacion)
+        Controls.Add(txtStockMaximo)
+        Controls.Add(txtCantidadStock)
+        Controls.Add(txtIdInventario)
+        Controls.Add(txtIdAlmacen)
+        Controls.Add(txtIdProducto)
+        Controls.Add(txtStockMinimo)
+        Name = "frmInventario"
+        Text = "frmInventario"
+        CType(dgvInventario, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtStockMinimo As TextBox
+    Friend WithEvents txtIdProducto As TextBox
+    Friend WithEvents txtIdAlmacen As TextBox
+    Friend WithEvents txtIdInventario As TextBox
+    Friend WithEvents txtCantidadStock As TextBox
+    Friend WithEvents txtStockMaximo As TextBox
+    Friend WithEvents dtpFechaActualizacion As DateTimePicker
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnCerrar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents dgvInventario As DataGridView
+End Class
---
+++ AppBimbo/frmInventario.vb
@@ -0,0 +1,192 @@
+﻿Imports System.Data.SqlClient
+Imports Microsoft.Data.SqlClient
+
+Public Class frmInventario
+
+    ' 🔗 Cadena de conexión a tu base de datos BimboDB
+    Private connectionString As String = "Server=LAPTOP-2A9AH3M7;Database=BimboDB;Integrated Security=True;TrustServerCertificate=True;"
+
+    ' ============ MÉTODOS AUXILIARES ============
+
+    Private Sub frmInventario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ConfigurarControles()
+        CargarInventario()
+    End Sub
+
+    Private Sub ConfigurarControles()
+        txtIdInventario.ReadOnly = True
+        dtpFechaActualizacion.Enabled = False   ' La fecha la pone SQL con GETDATE()
+
+        dgvInventario.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+        dgvInventario.MultiSelect = False
+        dgvInventario.ReadOnly = True
+        dgvInventario.AllowUserToAddRows = False
+    End Sub
+
+    Private Sub LimpiarCampos()
+        txtIdInventario.Text = ""
+        txtIdProducto.Text = ""
+        txtIdAlmacen.Text = ""
+        txtCantidadStock.Text = ""
+        txtStockMinimo.Text = ""
+        txtStockMaximo.Text = ""
+        dtpFechaActualizacion.Value = DateTime.Now
+    End Sub
+
+    Private Sub CargarInventario()
+        Try
+            Using cn As New SqlConnection(connectionString)
+                Dim da As New SqlDataAdapter("SELECT id_inventario, id_producto, id_almacen, cantidad_stock, stock_minimo, stock_maximo, fecha_actualizacion FROM Inventario", cn)
+                Dim dt As New DataTable()
+                da.Fill(dt)
+                dgvInventario.DataSource = dt
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error al cargar inventario: " & ex.Message)
+        End Try
+    End Sub
+
+    ' ============ EVENTOS DE LA GRILLA ============
+
+    Private Sub dgvInventario_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvInventario.CellClick
+        If e.RowIndex >= 0 AndAlso dgvInventario.Rows.Count > 0 Then
+            Dim fila As DataGridViewRow = dgvInventario.Rows(e.RowIndex)
+
+            txtIdInventario.Text = fila.Cells("id_inventario").Value.ToString()
+            txtIdProducto.Text = fila.Cells("id_producto").Value.ToString()
+            txtIdAlmacen.Text = fila.Cells("id_almacen").Value.ToString()
+            txtCantidadStock.Text = fila.Cells("cantidad_stock").Value.ToString()
+            txtStockMinimo.Text = fila.Cells("stock_minimo").Value.ToString()
+            txtStockMaximo.Text = fila.Cells("stock_maximo").Value.ToString()
+
+            If Not IsDBNull(fila.Cells("fecha_actualizacion").Value) Then
+                dtpFechaActualizacion.Value = Convert.ToDateTime(fila.Cells("fecha_actualizacion").Value)
+            End If
+        End If
+    End Sub
+
+    ' ============ BOTONES ============
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        LimpiarCampos()
+        txtIdProducto.Focus()
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        ' INSERT
+        Try
+            Using cn As New SqlConnection(connectionString)
+                cn.Open()
+
+                Dim sql As String = "INSERT INTO Inventario (id_producto, id_almacen, cantidad_stock, stock_minimo, stock_maximo) " &
+                                    "VALUES (@id_producto, @id_almacen, @cantidad_stock, @stock_minimo, @stock_maximo);"
+
+                Using cmd As New SqlCommand(sql, cn)
+                    cmd.Parameters.AddWithValue("@id_producto", txtIdProducto.Text)
+                    cmd.Parameters.AddWithValue("@id_almacen", txtIdAlmacen.Text)
+                    cmd.Parameters.AddWithValue("@cantidad_stock", txtCantidadStock.Text)
+                    cmd.Parameters.AddWithValue("@stock_minimo", txtStockMinimo.Text)
+                    cmd.Parameters.AddWithValue("@stock_maximo", txtStockMaximo.Text)
+
+                    cmd.ExecuteNonQuery()
+                End Using
+            End Using
+
+            MessageBox.Show("Inventario registrado correctamente.")
+            CargarInventario()
+            LimpiarCampos()
+
+        Catch ex As SqlException
+            MessageBox.Show("Error al guardar. Verifica que el id_producto y el id_almacen existan." &
+                            Environment.NewLine & ex.Message)
+        Catch ex As Exception
+            MessageBox.Show("Error al guardar: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+        ' UPDATE
+        If String.IsNullOrWhiteSpace(txtIdInventario.Text) Then
+            MessageBox.Show("Seleccione un registro de la tabla para editar.")
+            Return
+        End If
+
+        Try
+            Using cn As New SqlConnection(connectionString)
+                cn.Open()
+
+                Dim sql As String = "UPDATE Inventario SET " &
+                                    "id_producto = @id_producto, " &
+                                    "id_almacen = @id_almacen, " &
+                                    "cantidad_stock = @cantidad_stock, " &
+                                    "stock_minimo = @stock_minimo, " &
+                                    "stock_maximo = @stock_maximo, " &
+                                    "fecha_actualizacion = GETDATE() " &
+                                    "WHERE id_inventario = @id_inventario;"
+
+                Using cmd As New SqlCommand(sql, cn)
+                    cmd.Parameters.AddWithValue("@id_inventario", txtIdInventario.Text)
+                    cmd.Parameters.AddWithValue("@id_producto", txtIdProducto.Text)
+                    cmd.Parameters.AddWithValue("@id_almacen", txtIdAlmacen.Text)
+                    cmd.Parameters.AddWithValue("@cantidad_stock", txtCantidadStock.Text)
+                    cmd.Parameters.AddWithValue("@stock_minimo", txtStockMinimo.Text)
+                    cmd.Parameters.AddWithValue("@stock_maximo", txtStockMaximo.Text)
+
+                    cmd.ExecuteNonQuery()
+                End Using
+            End Using
+
+            MessageBox.Show("Inventario actualizado correctamente.")
+            CargarInventario()
+
+        Catch ex As SqlException
+            MessageBox.Show("Error al actualizar. Revisa las claves foráneas o los datos numéricos." &
+                            Environment.NewLine & ex.Message)
+        Catch ex As Exception
+            MessageBox.Show("Error al actualizar: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        ' DELETE
+        If String.IsNullOrWhiteSpace(txtIdInventario.Text) Then
+            MessageBox.Show("Seleccione un registro para eliminar.")
+            Return
+        End If
+
+        Dim r = MessageBox.Show("¿Seguro que deseas eliminar este registro?", "Confirmar",
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+
+        If r <> DialogResult.Yes Then
+            Return
+        End If
+
+        Try
+            Using cn As New SqlConnection(connectionString)
+                cn.Open()
+
+                Dim sql As String = "DELETE FROM Inventario WHERE id_inventario = @id_inventario;"
+
+                Using cmd As New SqlCommand(sql, cn)
+                    cmd.Parameters.AddWithValue("@id_inventario", txtIdInventario.Text)
+                    cmd.ExecuteNonQuery()
+                End Using
+            End Using
+
+            MessageBox.Show("Registro eliminado correctamente.")
+            CargarInventario()
+            LimpiarCampos()
+
+        Catch ex As SqlException
+            MessageBox.Show("No se pudo eliminar el registro (puede estar relacionado con otros datos)." &
+                            Environment.NewLine & ex.Message)
+        Catch ex As Exception
+            MessageBox.Show("Error al eliminar: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
+        Me.Close()
+    End Sub
+
+End Class
